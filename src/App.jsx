45ef7740fcdb8f34158b6ba8eea3192927b9fede
sn0w-m0nkey <148561536+sn0w-m0nkey@users.noreply.github.com>
2025-07-03 13:32:01 +0100

    Creating & displaying todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
-import { useState } from 'react'
-import { TodoForm } from './components/TodoForm/TodoForm'
-import styles from './App.module.css'
+import { useState } from "react";
+import { TodoForm } from "./components/TodoForm/TodoForm";
+import { TodoList } from "./components/TodoList/TodoList";
+import styles from "./App.module.css";
+
+const TODOS_DEFAULT = [
+  {
+    id: "1",
+    name: "Buy an Ice Cream",
+    description: "The white one with chocolate",
+    deadline: "2025-02-09",
+    priority: "low",
+    completed: false,
+  },
+  {
+    id: "2",
+    name: "Sell old MacBook Pro 2025",
+    description: "Try to sell it on OLX",
+    deadline: "2025-02-28",
+    priority: "high",
+    completed: false,
+  },
+  {
+    id: "3",
+    name: "Charge Powerbank",
+    description: "For the next travelling",
+    deadline: "2025-02-15",
+    priority: "medium",
+    completed: true,
+  },
+  {
+    id: "4",
+    name: "Test Todo only with a name",
+    description: "",
+    deadline: "",
+    priority: "none",
+    completed: false,
+  },
+];
 
 function App() {
-  const [ todos, setTodos ] = useState([])
-  
+  const [todos, setTodos] = useState(TODOS_DEFAULT);
+
   function handleCreate(newTodo) {
     setTodos((prevTodos) => [
       ...prevTodos,
@@ -12,19 +48,19 @@ function App() {
     ]);
   }
 
-  return ( 
+  return (
     <div className={styles.App}>
-    <header className={styles.Header}>
-      <img className={styles.Logo} src='/to-do.png' />
-      <h2 className={styles.Title}>To-Do App</h2>
+      <header className={styles.Header}>
+        <img className={styles.Logo} src="/to-do.png" />
+        <h2 className={styles.Title}>To-Do App</h2>
       </header>
 
       <div className={styles.AppContainer}>
         <TodoForm onCreate={handleCreate} />
-        {JSON.stringify(todos)}
+        <TodoList todos={todos} />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
